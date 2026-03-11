e2f5bb752b60669f5b5f05fc7ca52e7cfd2bd539
Dwayne Passenz <164025869+Dwayne-PassenzSt10453003@users.noreply.github.com>
2026-03-11 10:33:31 +0200

    Updated code

--- src/QuickServeApp.java
+++ src/QuickServeApp.java
@@ -4,6 +4,44 @@ public class QuickServeApp {
      
     public static void main(String[] args) {
         
-        JOptionPane.showMessageDialog(null, "Program started!");
+       //Collecting user input
+       String name = JOptionPane.showInputDialog("Enter Customer Name:");
+       String studentNumber = JOptionPane.showInputDialog("Enter studentNumber:");
+       String item = JOptionPane.showInputDialog("Enter item orderd:");
+       
+       int quantity = Integer.parseInt(
+              JOptionPane.showInputDialog("Enter quantity:")
+       );
+       double price = Double.parseDouble(
+              JOptionPane.showInputDialog("Enter price per irem:")
+       );
+       
+       //Create order object
+       Order order = new Order(name, studentNumber, item, quantity, price);
+       
+       //Perform calculations
+       double subtotal = order.calculateSubtotal();
+       double discount = order.calculateDiscount();
+       double vat = order.calculateVat();
+       double total = order.calculateTotal();
+       
+       //Producing the receipt
+       String receipt = 
+          "Campus QuickServe Receipt\n\n"
+               + "Customer Name: " + order.getCustomerName() + "\n"
+               + "Student Number:" + order.getStudentNumber() + "\n"
+               + "item Orderd:" + item + "\n"
+               + "Quantity:" + quantity + "\n"
+               + "Price per item: R" + price + "\n\n"
+               + "Subtotal: R" + subtotal + "\n"
+               + "Discount Applied: R" + discount + "\n"
+               + "VAT (15%): R" + vat + "\n"
+               + "Total: R" + total;
+       
+       //Display receipt
+       JOptionPane.showMessagedialog(null, receipt);
+       
+       system.exit(0);
+                  
     }
 }
